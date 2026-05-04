31a2d363f993a0096de282162178d78bc2462065
Drew Cochran <cochandr@oregonstate.edu>
2026-05-04 16:15:58 +0700

    Added color to puzzle pieces. Added win condition function in PuzzleManager.cs script. Added win conditions function Winner() to each ATM.

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -11,6 +11,15 @@ private int grayATMCounter;
 private int blueATMCounter;
 private int purpleATMCounter;
 
+// win condition function
+public void Winner()
+    {
+        if (graypuzzlePieces[1].activeInHierarchy && bluepuzzlePieces[0].activeInHierarchy && purplepuzzlePieces[2].activeInHierarchy)
+        {
+            Debug.Log("Player has won the game!");
+        }
+    }
+
 public void grayATM()
     {
         grayATMCounter++;
@@ -41,6 +50,74 @@ public void grayATM()
                 graypuzzlePieces[2].SetActive(false);
                 grayATMCounter = 0;
                 Debug.Log("Gray ATM interacted with. Counter: " + grayATMCounter);
+                break;
+        }
+    }
+
+public void blueATM()
+    {
+        blueATMCounter++;
+
+        switch (blueATMCounter)
+        {
+            case 0:
+                bluepuzzlePieces[0].SetActive(true);
+                bluepuzzlePieces[1].SetActive(false);
+                bluepuzzlePieces[2].SetActive(false);
+                Debug.Log("Blue ATM interacted with. Counter: " + blueATMCounter);
+                break;
+            case 1:
+                bluepuzzlePieces[1].SetActive(true);
+                bluepuzzlePieces[0].SetActive(false);
+                bluepuzzlePieces[2].SetActive(false);
+                Debug.Log("Blue ATM interacted with. Counter: " + blueATMCounter);
+                break;
+            case 2:
+                bluepuzzlePieces[2].SetActive(true);
+                bluepuzzlePieces[0].SetActive(false);
+                bluepuzzlePieces[1].SetActive(false);
+                Debug.Log("Blue ATM interacted with. Counter: " + blueATMCounter);
+                break;
+            case 3:
+                bluepuzzlePieces[0].SetActive(true);
+                bluepuzzlePieces[1].SetActive(false);
+                bluepuzzlePieces[2].SetActive(false);
+                blueATMCounter = 0;
+                Debug.Log("Blue ATM interacted with. Counter: " + blueATMCounter);
+                break;
+        }
+    }
+
+public void purpleATM()
+    {
+        purpleATMCounter++;
+
+        switch (purpleATMCounter)
+        {
+            case 0:
+                purplepuzzlePieces[0].SetActive(true);
+                purplepuzzlePieces[1].SetActive(false);
+                purplepuzzlePieces[2].SetActive(false);
+                Debug.Log("Purple ATM interacted with. Counter: " + purpleATMCounter);
+                break;
+            case 1:
+                purplepuzzlePieces[1].SetActive(true);
+                purplepuzzlePieces[0].SetActive(false);
+                purplepuzzlePieces[2].SetActive(false);
+                Debug.Log("Purple ATM interacted with. Counter: " + purpleATMCounter);
+                break;
+            case 2:
+                purplepuzzlePieces[2].SetActive(true);
+                purplepuzzlePieces[0].SetActive(false);
+                purplepuzzlePieces[1].SetActive(false);
+                Debug.Log("Purple ATM interacted with. Counter: " + purpleATMCounter);
+                break;
+            case 3:
+                purplepuzzlePieces[0].SetActive(true);
+                purplepuzzlePieces[1].SetActive(false);
+                purplepuzzlePieces[2].SetActive(false);
+                purpleATMCounter = 0;
+                Debug.Log("Purple ATM interacted with. Counter: " + purpleATMCounter);
                 break;
         }
     }
