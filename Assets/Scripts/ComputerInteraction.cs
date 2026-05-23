cdd403605928269c5d1cea64b4489c2f3d90298f
Drew Cochran <79421191+drewber78@users.noreply.github.com>
2026-05-23 18:17:58 +0700

    Updated Drew's puzzle room. Minor debug changes. Working on ComputerInteraction and NumberGuessingGame scripts.

--- Assets/Scripts/ComputerInteraction.cs
+++ Assets/Scripts/ComputerInteraction.cs
@@ -1,16 +1,74 @@
+
+/*
+Andrew J Cochran
+
+Computer Interaction script. Allows the player to interact with the computer in the escape room.
+Will trigger the computer screen to appear and allow the player to interact with it, then
+opens a number guessing game for the player to solve.
+
+*/
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class ComputerInteraction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public GameObject computerScreen;
+   public UnityEvent onComputerInteraction;
+
+   private escapeRoomControls inputActions;
+    private bool playerInRange;
+
+    private void Awake()
     {
+        inputActions = new escapeRoomControls();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Interact.performed += OnInteractPerformed;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Interact.performed -= OnInteractPerformed;
+        inputActions.Player.Disable();
+    }
+
+    private void OnInteractPerformed(InputAction.CallbackContext context)
+    {
+        if (!playerInRange)
+            return;
+
+        Debug.Log("Player is interacting with the computer.");
+        // null conditional operator to check if onComputerInteraction is not null before invoking it
+        // same as below code
+            if (onComputerInteraction != null)
+            {
+                onComputerInteraction.Invoke();
+            }
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!other.CompareTag("Player"))
+            return;
+
+        playerInRange = true;
+        //LeanTween.scale(computerScreen, Vector3.one, 2).setEaseInBounce();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        playerInRange = false;
+        //LeanTween.scale(computerScreen, Vector3.zero, 2).setEaseInBounce();
     }
 }
--- Assets/Scripts/NumberGuessingGame.cs
+++ Assets/Scripts/NumberGuessingGame.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
 
 public class NumberGuessingGame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // This script will be attached to the computer screen in the escape room. It will handle the number guessing game logic.
+    // local variables
+    private int randomNumber;
+    private int playerGuess;
+    public UnityEvent OnCorrectGuess;
+    private void Start()
     {
-        
+        // Generate a random number between 1 and 100
+        randomNumber = Random.Range(1, 101);
+        Debug.Log("Random Number (for testing): " + randomNumber);
     }
+
+    
+    
+    
 
     // Update is called once per frame
     void Update()
