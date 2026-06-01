c1480b7026d05c9c1b40612f34fcae83f82e94dd
Drew Cochran <79421191+drewber78@users.noreply.github.com>
2026-06-01 10:38:18 +0700

    Deleted unused code from SceneLoader.cs.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,9 +5,5 @@ public class SceneLoader : MonoBehaviour
  [SerializeField] private SceneFlowManager SceneFlowManager;
  [SerializeField] public string sceneToLoad;
 
-/*     public void OnClick()
-    {
-        SceneFlowManager.LoadScene(sceneToLoad);
-        Debug.Log("SceneLoader: OnClick called, loading scene: " + sceneToLoad);
-    } */
+
 }
